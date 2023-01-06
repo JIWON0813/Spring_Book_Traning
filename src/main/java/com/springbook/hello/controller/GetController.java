9223fceb76b1bfc5758d776c8936a80275c74fda
JIWON KIM <kkjim1324@naver.com>
2023-01-07 01:35:10 +0900

    feat : logger 실습

--- src/main/java/com/springbook/hello/controller/GetController.java
+++ src/main/java/com/springbook/hello/controller/GetController.java
@@ -1,6 +1,8 @@
 package com.springbook.hello.controller;
 
 import com.springbook.hello.dto.MemberDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -9,8 +11,11 @@ import java.util.Map;
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
+        LOGGER.info("hello 메서드가 호출되었습니다.");
         return "Hello World";
     }
 
